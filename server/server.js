b2668a9e672ffdc2b06ffd079533fc4aa909a2ad
Maggie Crocamo <mjcrocamo@gmail.com>
2019-12-08 14:24:56 -0500

    Updating some settings

--- server/server.js
+++ server/server.js
@@ -80,15 +80,13 @@ app.use(
 );
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client'));
-  app.get('*', function(req, res) {
-    res.sendFile(path.resolve(__dirname, '../client/index.html'), function(
-      err
-    ) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    });
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
