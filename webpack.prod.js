a2f51e5f43935815c0edf6e6d3fde32b62bb01ad
Maggie Crocamo <mjcrocamo@gmail.com>
2019-12-08 16:22:51 -0500

    Webpack prod configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -21,7 +21,7 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
