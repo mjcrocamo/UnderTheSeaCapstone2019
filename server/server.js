754a42982f7693c5bcb97a14c973215cb99b9f40
Maggie Crocamo <mjcrocamo@gmail.com>
2019-12-08 14:39:59 -0500

    Updating some settings

--- server/server.js
+++ server/server.js
@@ -79,17 +79,6 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  app.use(express.static('client'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'index.html'));
-  });
-}
-
 if (process.env.NODE_ENV === 'development') {
   app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
   app.use(webpackMiddleware(webpack(webpackConfig)));
